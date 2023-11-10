617bb1e9fe5c6bd2be29ac253ff1e42d24d67df0
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2023-11-10 20:28:24 +0700

    Update gradle & update token

--- core/src/main/java/com/example/core/di/CoreModule.kt
+++ core/src/main/java/com/example/core/di/CoreModule.kt
@@ -6,60 +6,98 @@ import com.example.core.BuildConfig
 import com.example.core.data.UserRepository
 import com.example.core.data.source.local.LocalDataSource
 import com.example.core.data.source.local.preferences.SharedPreferences
-import com.example.core.data.source.local.room.UserDao
 import com.example.core.data.source.local.room.UserDatabase
 import com.example.core.data.source.remote.RemoteDataSource
 import com.example.core.data.source.remote.network.ApiService
 import com.example.core.domain.repository.IUserRepository
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-private const val API_KEY = BuildConfig.KEY
-private const val BASE_URL = BuildConfig.BASE_URL
-
 val databaseModule = module {
-    factory { provideUserDao(get()) }
-    single { provideUserDatabase(androidContext()) }
+    factory { get<UserDatabase>().userDao() }
+
+    single {
+        provideUserDatabase(androidContext())
+    }
 }
 
-private fun provideUserDao(database: UserDatabase): UserDao {
-    return database.userDao()
-}
+fun provideUserDatabase(context: Context): UserDatabase {
+    val passphrase: ByteArray = SQLiteDatabase.getBytes("users".toCharArray())
+    val factory = SupportFactory(passphrase)
 
-private fun provideUserDatabase(context: Context): UserDatabase {
     return Room.databaseBuilder(
         context,
-        UserDatabase::class.java, "User.db"
-    ).fallbackToDestructiveMigration().build()
+        UserDatabase::class.java,
+        "Users.db"
+    ).fallbackToDestructiveMigration()
+        .openHelperFactory(factory)
+        .build()
 }
 
+
 val networkModule = module {
-    factory { provideOkHttpClient() }
-    factory { provideRetrofit(get()) }
+    single { provideAuthInterceptor() }
+    single { provideLoggingInterceptor() }
+    single { provideCertificatePinner() }
+    single { provideOkHttpClient(get(), get(), get()) }
+    single { provideRetrofit(get()) }
     single { provideApiService(get()) }
 }
 
-fun provideOkHttpClient(): OkHttpClient {
+fun provideAuthInterceptor(): Interceptor {
+    return Interceptor { chain ->
+        val req = chain.request()
+        val requestHeaders = req.newBuilder()
+            .addHeader("Authorization", "token " + BuildConfig.KEY)
+            .build()
+        chain.proceed(requestHeaders)
+    }
+}
+
+fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+    return if (BuildConfig.DEBUG) {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    } else {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+    }
+}
+
+fun provideCertificatePinner(): CertificatePinner {
+    val hostname = "api.github.com"
+    return CertificatePinner.Builder()
+        .add(hostname, "sha256/jFaeVpA8UQuidlJkkpIdq3MPwD0m8XbuCRbJlezysBE=")
+        .add(hostname, "sha256/Wec45nQiFwKvHtuHxSAMGkt19k+uPSw9JlEkxhvYPHk=")
+        .add(hostname, "sha256/Jg78dOE+fydIGk19swWwiypUSR6HWZybfnJG/8G7pyM=")
+        .add(hostname, "sha256/e0IRz5Tio3GA1Xs4fUVWmH1xHDiH2dMbVtCBSkOIdqM=")
+        .build()
+}
+
+fun provideOkHttpClient(
+    loggingInterceptor: HttpLoggingInterceptor,
+    authInterceptor: Interceptor,
+    certificatePinner: CertificatePinner
+): OkHttpClient {
     return OkHttpClient.Builder()
-        .addInterceptor { chain ->
-            val original = chain.request()
-            val requestBuilder = original.newBuilder()
-                .header("Authorization", API_KEY)
-            val request = requestBuilder.build()
-            chain.proceed(request)
-        }
-        .connectTimeout(1, TimeUnit.MINUTES)
-        .readTimeout(1, TimeUnit.MINUTES)
+        .addInterceptor(loggingInterceptor)
+        .addInterceptor(authInterceptor)
+        .connectTimeout(120, TimeUnit.SECONDS)
+        .readTimeout(120, TimeUnit.SECONDS)
+        .certificatePinner(certificatePinner)
         .build()
 }
 
 fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
     return Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl(BuildConfig.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .client(okHttpClient)
         .build()
